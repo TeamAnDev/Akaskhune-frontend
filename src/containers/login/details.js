caf445cae6b6fd236658383b23c0d9f49932c7e0
Hamed_Kh99 <goo.hamedacc@gmail.com>
2018-08-18 15:50:28 +0430

    details of login page added

---
+++ src/containers/login/details.js
@@ -0,0 +1,26 @@
+import {StyleSheet, Text, View, Image} from 'react-native'
+
+const Details = () => {
+    return (
+        <View>
+            <Image source={require('../src/containers/login/logo.jpg')} style={styles.logo}/>
+            <Text style={styles.name}>عکاسخونه</Text>
+        </View>
+    )
+} 
+
+const styles = StyleSheet.create({
+    logo : {
+        width : 50,
+        height : 50,
+        borderRadius : 50
+    },
+    name : {
+        marginTop : 10
+    },
+    container : {
+        flex : 1,
+        justifyContent : 'center',
+        alignItems: 'center'
+    }
+})
