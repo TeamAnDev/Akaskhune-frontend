518f185cc3caa35a4d142b3394a19003517f3779
Hamed_Kh99 <goo.hamedacc@gmail.com>
2018-08-19 10:33:28 +0430

    footer of login done

--- src/containers/login/Footer.js
+++ src/containers/login/Footer.js
@@ -1,8 +1,21 @@
 import {FHButton} from '../src/components'
-
+import {View, StyleSheet} from 'react-native'
 const Footer = () => {
     return (
-        <FHButton title = 'ورود با حساب گوگل' onPress = {() => console.log("log in with google")}/>
-        
+        <View style={styles.container}>
+            <FHButton title = 'ورود با حساب گوگل' onPress = {() => console.log("log in with google")}/>
+            <Text style = {styles.text} onPress = {() => console.log("sign up")}>ثبت نام</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1
+    },
+    text : {
+        marginTop : 10
+    }
+})
+
+export default Footer;
