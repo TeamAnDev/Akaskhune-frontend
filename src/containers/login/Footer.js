a3e0bd48f4af476ee8b293c8045c54cf4cf7578e
Hamed_Kh99 <goo.hamedacc@gmail.com>
2018-08-19 10:11:15 +0430

    login footer started

---
+++ src/containers/login/Footer.js
@@ -0,0 +1,8 @@
+import {FHButton} from '../src/components'
+
+const Footer = () => {
+    return (
+        <FHButton title = 'ورود با حساب گوگل' onPress = {() => console.log("log in with google")}/>
+        
+    )
+}
