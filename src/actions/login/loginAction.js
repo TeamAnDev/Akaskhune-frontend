a156e5d333f86944496ef9a5174074551e943ff7
Hamed_Kh99 <goo.hamedacc@gmail.com>
2018-08-20 14:38:37 +0430

    request for login

--- src/actions/login/loginAction.js
+++ src/actions/login/loginAction.js
@@ -1,17 +1,17 @@
-export const EMAIL_CHANGED = 'EMAIL_CHANGED';
-export const PASSWORD_CHANGED = 'PASSWORD_CHANGED';
+export const LOGIN_EMAIL_CHANGED = 'EMAIL_CHANGED';
+export const LOGIN_PASSWORD_CHANGED = 'PASSWORD_CHANGED';
 export const LOGIN = 'LOGIN';
 export const LOGIN_GOOGLE = 'LOGIN_GOOGLE'
 export const SIGNUP_PAGE = 'SIGNUP_PAGE'
 export function changeEmail(email){
     return {
-        type : EMAIL_CHANGED,
+        type : LOGIN_EMAIL_CHANGED,
         email
     }
 }
 export function changePassword(password){
     return {
-        type : PASSWORD_CHANGED,
+        type : LOGIN_PASSWORD_CHANGED,
         password
     }
 }
--- src/reducers/login/login.js
+++ src/reducers/login/login.js
@@ -1,21 +1,28 @@
-import {EMAIL_CHANGED , PASSWORD_CHANGED, LOGIN, LOGIN_GOOGLE, SIGNUP, SIGNUP_PAGE} from '../../actions/login/loginAction';
+import {LOGIN_EMAIL_CHANGED , LOGIN_PASSWORD_CHANGED, LOGIN, LOGIN_GOOGLE, SIGNUP, SIGNUP_PAGE} from '../../actions/login/loginAction';
 import {combineReducers} from 'redux';
 import errors from "../../config/errors";
+import axios from 'axios';
 function loginReducer(state={email:"", password:"", login:false, token:"", err:""}, action)
 {
     switch(action.type){
-        case(EMAIL_CHANGED):
+        case(LOGIN_EMAIL_CHANGED):
             return Object.assign({}, state, {email : action.email});
-        case(PASSWORD_CHANGED):
+        case(LOGIN_PASSWORD_CHANGED):
             return Object.assign({}, state, {password : action.password});
         case(LOGIN):
+            console.warn("reducer -> login")
             if(state.email!=="" && state.password!=="")
             {
-                let response = undefined;
+                let response = {};
+                axios.get('http://localhost:3000/login')
+                    .then(response => console.warn(response))
+                    .catch(err => console.warn(err));
                 if(response.token)
                     return Object.assign({}, state, {login:true, token:token});
                 else if(response.err)
                     return Object.assign({}, state, {err:response.err});
+                else 
+                    return state;
             }
             else
                 return Object.assign({}, state, {err:errors.fillInputsLogin});
