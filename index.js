13e91a8bcd1dcbca7b70c40a19fd9dcc8d40f672
Farzad Habibi <farzadhabibii98@gmail.com>
2018-08-20 16:23:01 +0430

    redux-tunk added with axios requests

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import React from 'react'
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import AkaskhuneApp from './src/reducers/index';
-const store = createStore(AkaskhuneApp);
+import thunk from 'redux-thunk';
+
+const store = createStore(AkaskhuneApp, applyMiddleware(thunk));
 
 const Container = (props) => (
     <Provider store={store}>
