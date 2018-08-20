246b728327215d54a0888f7560598e5f9522b4a6
Farzad Habibi <farzadhabibii98@gmail.com>
2018-08-20 22:23:44 +0430

    button changed

--- src/components/FHButton.js
+++ src/components/FHButton.js
@@ -5,13 +5,14 @@ import {Button, Text , Spinner} from 'native-base';
 
 const FHButton = (props) => {
     const Loading = props.loading ? <Spinner animating={props.loading}/> : null;
-    return (<Button block primary onPress={props.onPress} disabled={props.disabled || false}
+    return (<Button block rounded primary onPress={props.onPress} disabled={props.disabled || false}
         style={{ margin : 7,
             height : 55,
             borderRadius: 10,
             width : "88%",
             alignContent:"center",
-            alignSelf: 'center'}}>
+            alignSelf: 'center',
+            justifyContent:"center"}}>
         <Text>{props.title}</Text>
         {Loading}
         </Button>)
--- src/config/urls.js
+++ src/config/urls.js
@@ -2,8 +2,13 @@ import axios from 'axios'
 
 export const rest = {
     login : '/login',
+    signup : '/signup'
 }
 
 export const axiosInstance = axios.create({
-    baseURL: ''
+    baseURL: 'http://localhost:3000/',
+    timeout:10000,
+    headers:{
+        'Content-Type': 'application/json',
+    }
 })
