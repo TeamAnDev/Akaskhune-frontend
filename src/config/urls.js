648731119779a9ac2eedd7856afa76b1ebec590d
Hamed_Kh99 <goo.hamedacc@gmail.com>
2018-08-21 17:06:04 +0430

    debugged and request for login completed TONE-9

--- src/config/urls.js
+++ src/config/urls.js
@@ -10,11 +10,7 @@ export const rest = {
 // axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export const axiosInstance = axios.create({
-<<<<<<< HEAD
-    baseURL: 'http://192.168.11.199:8080',
-=======
-    baseURL: 'http://192.168.11.214:8080',
->>>>>>> 6af7455051c976fab9851f24d3dd60b0bee04f5a
+    baseURL: 'http://192.168.11.199:8085',
     timeout:10000,
     headers:{
         'Content-Type': 'application/json',
