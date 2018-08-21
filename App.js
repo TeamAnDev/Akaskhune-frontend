3b3b4c011afd105aeef693aa26e115f458dbc846
Farzad Habibi <farzadhabibii98@gmail.com>
2018-08-21 16:52:27 +0430

    navigate added signupComplete profile added

--- App.js
+++ App.js
@@ -10,43 +10,29 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Login from './src/containers/login/Login';
 import Signup from './src/containers/signup/Signup';
+import SignupCompleteProfile from './src/containers/signupCompleteProfile/SignupCompleteProfile';
 import colors from './src/config/colors';
+import {createStackNavigator} from 'react-navigation';
 import { StyleProvider } from 'native-base';
 import getTheme from './native-base-theme/components';
 import akaskhune from './native-base-theme/variables/akaskhune';
 
+
+const Router = createStackNavigator({
+  Signup,
+  Login
+}, {
+  initialRouteName: "Login",
+  headerMode: "none"
+})
 export default class App extends Component {
   render() {
     return (
       <StyleProvider  style={getTheme(akaskhune)}>
       <View style={{flex:1}}>
-        <Login/>
-        {/* <LoginInputs/> */}
-        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text> */}
+        <Router/>
       </View>
       </StyleProvider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
