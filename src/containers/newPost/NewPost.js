2b9aeb77f1b5a2cc79e710bb414b71e450a5629e
Farzad Habibi <farzadhabibii98@gmail.com>
2018-08-29 09:15:41 +0430

    add camera priview

--- src/containers/newPost/NewPost.js
+++ src/containers/newPost/NewPost.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Component} from 'react';
 import {View, Text} from 'react-native';
 import FHHeader from '../../components/FHHeader'
+import CameraPriview from './CameraPreview';
+import colors from '../../config/colors';
 export default class NewPost extends Component{
     constructor(props){
         super(props);
@@ -12,7 +14,8 @@ export default class NewPost extends Component{
             <View style = {{flex:1}}>
                 <FHHeader customIcon = "x" customIconType = "Feather" title="گالری تصاویر"/>
                 <View style = {{flex:1}}>
-                    <View style={{flex:250, backgroundColor:'red'}} >
+                    <View style={{flex:250, backgroundColor:colors.grey}} >
+                        <CameraPriview/>
                     </View>
                     <View style={{flex:310}} >
                     </View>
