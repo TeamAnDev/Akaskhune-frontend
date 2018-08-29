61ebe463934bcf6e136a51e01f8a37bac84697ba
Farzad Habibi <farzadhabibii98@gmail.com>
2018-08-29 09:51:16 +0430

    camera priv added

---
+++ src/containers/newPost/CameraPreview.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Component} from 'react';
+import {View} from 'react-native';
+
+export default class CameraPreview extends Component{
+    constructor(props){
+        super(props);
+    }
+    render()
+    {
+        return(
+            <View style = {{flex:1}}>
+          
+            </View>
+        )
+    }
+}
