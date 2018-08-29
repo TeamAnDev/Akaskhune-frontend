10a3bf63fcccc1d28adf8ee4207353bdc9593242
Farzad Habibi <farzadhabibii98@gmail.com>
2018-08-29 10:21:09 +0430

    all packages corrected

--- src/containers/newPost/CameraPreview.js
+++ src/containers/newPost/CameraPreview.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Component} from 'react';
 import {View} from 'react-native';
+import {CameraKitCamera} from 'react-native-camera-kit';
 export default class CameraPreview extends Component{
     constructor(props){
         super(props);
@@ -9,7 +10,21 @@ export default class CameraPreview extends Component{
     {
         return(
             <View style = {{flex:1}}>
-          
+                <CameraKitCamera
+                ref={cam => this.camera = cam}
+                style={{
+                    width:"100%",
+                    height:"100%",
+                    backgroundColor: 'red'
+                }}
+                cameraOptions={{
+                    flashMode: 'auto',             // on/off/auto(default)
+                    focusMode: 'on',               // off/on(default)
+                    zoomMode: 'on',                // off/on(default)
+                    ratioOverlay:'3:4',            // optional, ratio overlay on the camera and crop the image seamlessly
+                    ratioOverlayColor: '#00000077' // optional
+                }}
+                />
             </View>
         )
     }
