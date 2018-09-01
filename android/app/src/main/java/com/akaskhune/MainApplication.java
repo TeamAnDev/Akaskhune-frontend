f281613721c768a47fd3a35fcc4676048d519297
Farzad Habibi <farzadhabibii98@gmail.com>
2018-09-01 14:17:36 +0430

    ios adding permissions

--- android/app/src/main/java/com/akaskhune/MainApplication.java
+++ android/app/src/main/java/com/akaskhune/MainApplication.java
@@ -3,6 +3,7 @@ package com.akaskhune;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wix.RNCameraKit.RNCameraKitPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import org.reactnative.camera.RNCameraPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraKitPackage(),
             new ReactNativeContacts(),
             new RNCameraPackage(),
             new PickerPackage(),
