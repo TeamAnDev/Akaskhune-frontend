7c8b7a7f2077e01fc37610aaedaa3ef7eaf47a39
Hamed_Kh99 <goo.hamedacc@gmail.com>
2018-09-11 16:03:12 +0430

    privacy button created

--- src/containers/setting/List.js
+++ src/containers/setting/List.js
@@ -1,15 +1,32 @@
 import React, {Component} from 'react';
 import {Text, View, TouchableOpacity} from 'react-native'
-import { Icon } from 'native-base';
+import { Icon , Switch} from 'native-base';
 import styles from './styles'
 
-const SettingList = (props) => {
-       return (
-            <TouchableOpacity style={styles.listItem} onPress={() => props.navigation.navigate('ChangePassword')}>
-                <Text>تغییر رمز عبور</Text>
-                <Icon name="arrow-back"/>
-            </TouchableOpacity>
-        )
-    }        
+class SettingList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isPrivate : false};
+    }
+
+    togglePrivacy = () => {
+        this.setState({isPrivate : !this.state.isPrivate});
+    }
+
+    render() {
+        return (
+            <View style={{flex:1, flexDirection:'column'}}>
+                 <TouchableOpacity style={styles.listItem} onPress={() => this.props.navigation.navigate('ChangePassword')}>
+                     <Text>تغییر رمز عبور</Text>
+                     <Icon name="arrow-back"/>
+                 </TouchableOpacity>
+                 <TouchableOpacity style={styles.listItem} onPress={() => this.togglePrivacy()}>
+                     <Text>حساب شخصی</Text>
+                     <Switch value={this.state.isPrivate} onValueChange={() => this.togglePrivacy()}/>
+                 </TouchableOpacity>
+             </View>
+         )
+    }
+}  
 
 export default SettingList;
--- src/containers/setting/styles.js
+++ src/containers/setting/styles.js
@@ -1,15 +1,18 @@
 import {StyleSheet, Dimensions} from 'react-native';
 const marginTopOfCameraSelect = Dimensions.get("window").height * 3 / 100 ;
 const marginBottomOfCameraSelect = Dimensions.get("window").height * 3 / 100 ;
+const listHeight = Dimensions.get("window").height / 10;
+
 const styles = StyleSheet.create({
     listItem : {
-        flex:1,
+        // flex:1,
         justifyContent:'space-between', 
         flexDirection:'row-reverse', 
         padding:20, 
         borderWidth:1,
         borderColor:'#EFEFEF',
-        alignItems:'center'
+        alignItems:'center',
+        height: listHeight
     },
     cameraView : {
         marginTop : marginTopOfCameraSelect,
