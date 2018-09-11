27bfb21d7633b4fc5caaad1816b40b819350e33b
Farzad Habibi <farzadhabibii98@gmail.com>
2018-09-11 15:12:47 +0430

    bugs fixed

--- src/components/FHRow.js
+++ src/components/FHRow.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import {View, Image} from 'react-native';
+import {View, Image, TouchableOpacity} from 'react-native';
 import styles from '../containers/profile/styles';
+import {navigate} from '../../NavigationService';
 
 const FHRow = (props) => {
     return (
         <View style={{flex:1, flexDirection:'row', justifyContent:'space-between', marginHorizontal:10, marginVertical:10}}>
-            <Image style={styles.image} source={props.leftImage}/>
-            <Image style={styles.image} source={props.rightImage}/>
+            <TouchableOpacity onPress = {() => navigate('SinglePost', {id :props.leftImage.id})}>
+                <Image style={styles.image} source={props.leftImage}/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress = {() => navigate('SinglePost', {id :props.rightImage.id})}>
+                <Image style={styles.image} source={props.rightImage}/>
+            </TouchableOpacity>
+            
         </View>
     );
 }
