95ba0dd82b313c3bfe570a1f9a515d28e8d32e80
Hamed_Kh99 <goo.hamedacc@gmail.com>
2018-09-16 13:39:15 +0430

    some minor bug in popular tags

--- src/containers/search/PopularTagsPattern.js
+++ src/containers/search/PopularTagsPattern.js
@@ -14,12 +14,12 @@ const PopularTagsPattern = (props) => {
                     <PopularTag photo_url={props.tags[1] ? props.tags[1].photo_url : null} tagName={props.tags[1] ? props.tags[1].name : null} width={maxWidth / 2} height={maxHeight / 2}/>
                     <PopularTag photo_url={props.tags[2] ? props.tags[2].photo_url : null} tagName={props.tags[2] ? props.tags[2].name : null} width={maxWidth / 2} height={maxHeight / 2}/>
                 </View>
-                <PopularTag photo_url={props.tags[3] ? props.tags[1].photo_url : null} tagName={props.tags[3] ? props.tags[3].name : null} width={maxWidth / 2} height={maxHeight}/>
+                <PopularTag photo_url={props.tags[3] ? props.tags[3].photo_url : null} tagName={props.tags[3] ? props.tags[3].name : null} width={maxWidth / 2} height={maxHeight}/>
             </View>
-            <PopularTag photo_url={props.tags[4] ? props.tags[1].photo_url : null} tagName={props.tags[4] ? props.tags[4].name : null} width={maxWidth} height={maxHeight * 2 / 3}/>
+            <PopularTag photo_url={props.tags[4] ? props.tags[4].photo_url : null} tagName={props.tags[4] ? props.tags[4].name : null} width={maxWidth} height={maxHeight * 2 / 3}/>
             <View style={{flexDirection:'row'}}>
-                <PopularTag photo_url={props.tags[5] ? props.tags[2].photo_url : null} tagName={props.tags[5] ? props.tags[5].name : null} width={maxWidth / 2} height={maxHeight}/>
-                <PopularTag photo_url={props.tags[6] ? props.tags[0].photo_url : null} tagName={props.tags[6] ? props.tags[6].name : null} width={maxWidth / 2} height={maxHeight}/>
+                <PopularTag photo_url={props.tags[5] ? props.tags[5].photo_url : null} tagName={props.tags[5] ? props.tags[5].name : null} width={maxWidth / 2} height={maxHeight}/>
+                <PopularTag photo_url={props.tags[6] ? props.tags[6].photo_url : null} tagName={props.tags[6] ? props.tags[6].name : null} width={maxWidth / 2} height={maxHeight}/>
             </View>
         </View>
     )
